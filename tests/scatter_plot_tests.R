26129bce1a2338bac7ce838bd74f8b3a879ecc00
Vera Danilova <veradanilova765@gmail.com>
2023-03-11 21:25:07 -0800

    created tests for scatter plot function

---
+++ tests/scatter_plot_tests.R
@@ -0,0 +1,34 @@
+library(ggcheck)
+
+library(testthat)
+
+source("./R/scatter_plot.R")
+
+#test for input variables
+test_that("Function can only accept certain data types for arguments", {
+  expect_error(scatter_plot(iris, "Species", Sepal.Width, "Iris Species", "Sepal Width", "Scatter Plot", 10))
+  expect_error(scatter_plot(mtcars, mpg, hp, MPG, "HP", "Scatter Plot", 10))
+  expect_error(scatter_plot(mtcars, mpg, hp, "MPG", HP, "Scatter Plot", 10))
+  expect_error(scatter_plot(mtcars, mpg, hp, "MPG", "HP", Scatter , 10))
+  expect_error(scatter_plot(mtcars, mpg, hp, "MPG", "HP", "Scatter Plot", "10"))
+})
+
+
+#test for output variables
+test_that("Function returns a plot", {
+  expect_true(is.ggplot(scatter_plot(mtcars, mpg, hp, "MPG", "HP", "Scatter Plot", 10)))
+})
+
+
+
+test_that("Identifies labels correctly", {
+  expect_equal(
+    get_labels(scatter_plot(mtcars, mpg, hp, "MPG", "HP", "Scatter Plot", 10), c("title", "x", "y")), 
+    list(title = "Scatter Plot", x = "MPG", y = "HP")
+  )
+}
+)
+
+
+
+
