c1cf2ee5644b871d26a0afb5c35965be49070bbf
Jenna Le Noble <jenna_lenoble@hotmail.com>
2023-03-11 23:01:04 -0800

    added helper data for hist_plot

--- tests/hist_plot_tests.R
+++ tests/hist_plot_tests.R
@@ -15,13 +15,13 @@ test_that("Function can only accept certain data types for arguments", {
 
 #test for output variables
 test_that("Function returns a plot", {
-  expect_true(is.ggplot(hist_plot(mtcars, mpg, "MPG", "Count", "Histogram", 10)))
+  expect_true(is.ggplot(p1))
 })
 
 test_that("Identifies labels correctly", {
   expect_equal(
-    get_labels(hist_plot(mtcars, mpg, "mpg", "count", "histogram", 10), c("title", "x", "y")), 
-              list(title = "histogram", x = "mpg", y = "count")
+    get_labels(p1, c("title", "x", "y")), 
+              list(title = "Histogram", x = "Miles per Gallon", y = "Count")
     )
   }
 )
---
+++ tests/hist_plot_tests_helper.R
@@ -0,0 +1,5 @@
+# helper histogram plot for hist_plot()
+source("./R/hist_plot.R")
+p1 <- hist_plot(mtcars, mpg, "Miles per Gallon", "Count", "Histogram", 10)
+
+
